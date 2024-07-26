e7809c161d52ee2907dd2da192a14b955b3a85a8
Ankit Kumar Dam <aparnadam1981@gmail.com>
2024-07-27 00:26:47 +0530

    email

--- style-switcher.js
+++ style-switcher.js
@@ -35,3 +35,14 @@ dayNight.addEventListener("click",()=>{
 //     }
 // })
 
+
+function sendEmail() {
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const message = document.getElementById('myText').value;
+
+    const subject = document.getElementById('sub').value;
+    const body = encodeURIComponent(`Name: ${name}\nEmail: ${email}\nMessage: ${message}`);
+
+    window.location.href = `mailto:aparnadam1981@gmail.com?subject=${subject}&body=${body}`;
+}
