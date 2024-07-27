123e96b442bdb7969459e094864c2c572c12e360
Ankit Kumar Dam <aparnadam1981@gmail.com>
2024-07-28 02:46:33 +0530

    responsive

--- style-switcher.js
+++ style-switcher.js
@@ -1,3 +1,18 @@
+const dayNight = document.querySelector(".day-night");
+dayNight.addEventListener("click",()=>{
+    document.body.classList.toggle("dark");
+})
+
+function sendEmail() {
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const message = document.getElementById('myText').value;
+
+    const subject = document.getElementById('sub').value;
+    const body = encodeURIComponent(`Name: ${name}\nEmail: ${email}\nMessage: ${message}`);
+
+    window.location.href = `mailto:aparnadam1981@gmail.com?subject=${subject}&body=${body}`;
+}
 // const toggler = document.querySelector(".style-switcher-toggler");
 // toggler.addEventListener("click",()=>{
 //     document.querySelector(".style-switcher").classList.toggle("open");
@@ -24,10 +39,6 @@
 // });
 
 
-const dayNight = document.querySelector(".day-night");
-dayNight.addEventListener("click",()=>{
-    document.body.classList.toggle("dark");
-})
 // window.addEventListener("load",()=>{
 //     if(document.body.classList.contains("dark"))
 //     {
@@ -36,13 +47,3 @@ dayNight.addEventListener("click",()=>{
 // })
 
 
-function sendEmail() {
-    const name = document.getElementById('name').value;
-    const email = document.getElementById('email').value;
-    const message = document.getElementById('myText').value;
-
-    const subject = document.getElementById('sub').value;
-    const body = encodeURIComponent(`Name: ${name}\nEmail: ${email}\nMessage: ${message}`);
-
-    window.location.href = `mailto:aparnadam1981@gmail.com?subject=${subject}&body=${body}`;
-}
